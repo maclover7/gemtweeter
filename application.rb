250769544bcea7baa289db65e60cfc2e19ec22cf
Jon Moss <me@jonathanmoss.me>
2016-01-25 14:48:29 -0500

    rm `configure`

--- application.rb
+++ application.rb
@@ -17,16 +17,13 @@ class Whisper
 end
 
 class GemTweeter < Sinatra::Base
-  configure do
-    Log = Logger.new(STDOUT)
-    Log.level = Logger::INFO
-
-    $twitter = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-      config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
-    end
+  Log = Logger.new(STDOUT)
+  Log.level = Logger::INFO
+  $twitter = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+    config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+    config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+    config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
   end
 
   get '/' do
