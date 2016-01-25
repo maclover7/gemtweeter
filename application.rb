5e81a0fe93d72c68e6aa95cc03973bcb45124f8c
Jon Moss <me@jonathanmoss.me>
2016-01-25 14:34:53 -0500

    More readable ENV keys

--- application.rb
+++ application.rb
@@ -22,10 +22,10 @@ class GemTweeter < Sinatra::Base
     Log.level = Logger::INFO
 
     $twitter = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['CONSUMER_KEY']
-      config.consumer_secret     = ENV['CONSUMER_SECRET']
-      config.access_token        = ENV['REQUEST_TOKEN']
-      config.access_token_secret = ENV['REQUEST_SECRET']
+      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
     end
   end
 
