331e2f5516f85a4a1743aa028e8dbdd8fa8df565
Jon Moss <me@jonathanmoss.me>
2016-01-25 14:24:37 -0500

    Add functionality

--- Gemfile
+++ Gemfile
@@ -2,8 +2,10 @@ source 'https://rubygems.org'
 
 ruby '2.3.0'
 
+gem 'activesupport'
 gem 'puma'
 gem 'sinatra'
+gem 'twitter', '~> 5.9'
 
 group :development, :test do
   gem 'rack-test'
--- application.rb
+++ application.rb
@@ -1,6 +1,60 @@
 require 'sinatra'
+require 'logger'
+require 'active_support/core_ext/string/filters'
+require 'digest/sha2'
+
+class Whisper
+  attr_reader :name, :version, :url, :info
+
+  def initialize(hash)
+    @hash = @hash
+    ###
+    @name    = @hash['name']
+    @version = @hash['version']
+    @url     = @hash['project_uri']
+    @info    = @hash['info']
+  end
+end
 
 class GemTweeter < Sinatra::Base
+  configure do
+    Log = Logger.new(STDOUT)
+    Log.level = Logger::INFO
+
+    $twitter = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['CONSUMER_KEY']
+      config.consumer_secret     = ENV['CONSUMER_SECRET']
+      config.access_token        = ENV['REQUEST_TOKEN']
+      config.access_token_secret = ENV['REQUEST_SECRET']
+    end
+  end
+
   get '/' do
+    'It works!'
+  end
+
+  post '/hook' do
+    data = request.body.read
+    Log.info "got webhook: #{data}"
+
+    hash = JSON.parse(data)
+    Log.info "parsed json: #{hash.inspect}"
+
+    authorization = Digest::SHA2.hexdigest(hash['name'] + hash['version'] + ENV['RUBYGEMS_API_KEY'])
+    if env['HTTP_AUTHORIZATION'] == authorization
+      Log.info "authorized: #{env['HTTP_AUTHORIZATION']}"
+    else
+      Log.info "unauthorized: #{env['HTTP_AUTHORIZATION']}"
+      error 401
+    end
+
+    whisper = Whisper.new(hash)
+    Log.info "created whisper"
+
+    # Maximum length of a tweet sans URL is currently 140 characters - 22 (short URL length) - 1 space = 117 characters
+    whisper_text = "#{whisper.name} (#{whisper.version}): #{whisper.info}".truncate(117, omission: "…", separator: " ") + " #{whisper.url}"
+
+    response = $twitter.update(whisper_text)
+    Log.info "TWEETED! #{response}"
   end
 end
